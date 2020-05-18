a1eea34702f93e0824fa6eb91ef5af2dcd5b6f1c
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2020-05-18 13:14:40 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const { exec } = require("child_process");
+exec("osacompile", (error, stdout, stderr) => {
+    if (error) {
+        console.error(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
